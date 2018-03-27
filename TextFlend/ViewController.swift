7d59acabe91d14a7f73700cb4b05d509ef22a84c
csd5766 <csd5766@naver.com>
2018-03-27 15:34:41 +0900

    save

--- TextFlend/ViewController.swift
+++ TextFlend/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController,UITextFieldDelegate {
+    
     @IBOutlet weak var Hello: UILabel!
     @IBOutlet weak var TextField: UITextField!
     
@@ -16,9 +17,11 @@ class ViewController: UIViewController,UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // uiTextFieldDeleagte 객체와 viewConller 객체와 연결
-        TextField.delegate = self
+        //TextField.delegate = self
+        
         TextField.clearButtonMode = UITextFieldViewMode.always
         TextField.placeholder = "입력을 하세요"
+    
     }
     
     @IBAction func btn1(_ sender: Any) {
@@ -32,20 +35,19 @@ class ViewController: UIViewController,UITextFieldDelegate {
         view.endEditing(true)
 
     }
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ TextField: UITextField) -> Bool {
         print("I pressed return key")
+        Hello.text = "Hello" + TextField.text!
+        TextField.text = ""
+        TextField.resignFirstResponder()
+        return true
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
         view.backgroundColor = UIColor.yellow
-        textField.resignFirstResponder()
         return true
     }
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
         view.backgroundColor = UIColor.green
     }
-        override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-            }
-    }
-    
 
-
+}
